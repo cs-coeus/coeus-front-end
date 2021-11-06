4f037a0b009eca893ea8fa15063ad0ba4b11d279
zielu92 <zielu0@gmail.com>
2021-11-06 12:19:03 +0700

    feat: fix style

--- src/components/MapEditorPage/MapEditorBottom.vue
+++ src/components/MapEditorPage/MapEditorBottom.vue
@@ -30,37 +30,11 @@ h1 {
 .hero-action {
   position: relative;
   padding: 24px;
-  margin-top: 32px;
   background-color: var(--white);
   border-radius: 16px;
   box-shadow: 0 8px 24px 0 var(--black-a15);
   width: 50%;
   margin: auto;
-}
-
-.hero-action label {
-  font-size: 1.125rem;
-  font-weight: bold;
-}
-
-.hero-action  input {
-  margin-top: 8px;
-  width: 100%;
-  border: 1px solid var(--black-a20);
-  border-radius: 8px;
-  padding: 4px 8px;
-}
-
-.hero-action  p {
-  color: var(--grey);
-  margin-top: 8px;
-}
-
-.hero-cta {
-  font-size: 1.25rem;
-  position: absolute;
-  bottom: -16px;
-  right: -16px;
 }
 
 @media only screen and (max-width: 986px) {
